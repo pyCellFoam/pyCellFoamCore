709f09b3d4cedc9ef08e7465ec1c929ad79be5f1
Tobias Scheuermann <mail@tobias-scheuermann.de>
2023-09-27 12:54:43 +0200

    Formatted superCell file according to pep8

--- kCells/cell/superCell.py
+++ kCells/cell/superCell.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-#==============================================================================
+# =============================================================================
 # SUPER CELLL
-#==============================================================================
+# =============================================================================
 # Author:         Tobias Scheuermann
 # Institution:    Chair of Automatic Control
 #                 Department of Mechanical Engineering
@@ -13,145 +13,143 @@
 Parent class for all positive (simple) k-Cells.
 
 '''
-#==============================================================================
+# =============================================================================
 #    IMPORTS
-#==============================================================================
-#-------------------------------------------------------------------------
+# =============================================================================
+# ------------------------------------------------------------------------
 #    Change to Main Directory
-#-------------------------------------------------------------------------
+# ------------------------------------------------------------------------
 import os
 if __name__ == '__main__':
     os.chdir('../../')
 
-#-------------------------------------------------------------------------
+# ------------------------------------------------------------------------
 #    Local Libraries
-#-------------------------------------------------------------------------
+# ------------------------------------------------------------------------
 
 #    kCells
-#--------------------------------------------------------------------
+# -------------------------------------------------------------------
 from kCells.cell.superBaseCell import SuperBaseCell
 from kCells.cell.superReversedCell import SuperReversedCell
 
 
 #    Tools
-#--------------------------------------------------------------------
+# -------------------------------------------------------------------
 import tools.colorConsole as cc
 from tools import MyLogging
 
 
-#==============================================================================
+# =============================================================================
 #    CLASS DEFINITION
-#==============================================================================
+# =============================================================================
 
 class SuperCell(SuperBaseCell):
     '''
     This class only inherits from the SuperBaseCell class.
-    
+
     '''
-    
-#==============================================================================
+
+# =============================================================================
 #    INITIALIZATION
-#==============================================================================
-    def __init__(self,*args,myReverse=None,**kwargs):   
+# =============================================================================
+    def __init__(self, *args, myReverse=None, **kwargs):
         '''
-        
-        
-        :param SuperBaseCell myReverse: 
-        :param str loggerName: The logger name needs to be passed from the 
+
+
+        :param SuperBaseCell myReverse:
+        :param str loggerName: The logger name needs to be passed from the
             class at the lowest level by loggerName = __name__
         '''
-        if myReverse == None:
-            myReverse = SuperReversedCell(myReverse=self,**kwargs)
-            
+        if myReverse is None:
+            myReverse = SuperReversedCell(myReverse=self, **kwargs)
+
         self.__isDeleted = False
-        super().__init__(*args,myReverse=myReverse,**kwargs)
-        
+        super().__init__(*args, myReverse=myReverse, **kwargs)
+
         self.logger.debug('Initialized SuperCell')
-        
-        
-    
-#==============================================================================
+
+# =============================================================================
 #    SETTER AND GETTER
-#==============================================================================
+# =============================================================================
+
     def __getLabelPrefix(self): return ''
+
     labelPrefix = property(__getLabelPrefix)
     '''
     Positive k-cells get no prefix.
-    
+
     '''
-    
-    
+
     def __getIsReverse(self): return False
+
     isReverse = property(__getIsReverse)
     '''
     All child k-cells are positive, therefor this is always false.
-    
+
     '''
-    
-    
+
     def __getIsDeleted(self): return self.__isDeleted
+
     isDeleted = property(__getIsDeleted)
     '''
     To keep track of deleted cells, they are not completely removed from
     memory but marked as "deleted"
-    
+
     '''
 
-
-
-#-------------------------------------------------------------------------
+# ------------------------------------------------------------------------
 #    Standard values for properties that will be defined in child classes
 #    but that are needed for methods in this class
-#-------------------------------------------------------------------------
-    
-    def __getLabel(self): 
+# ------------------------------------------------------------------------
+
+    def __getLabel(self):
         self.logger.warning('Using standard value for label')
         return 'SUPC'
+
     label = property(__getLabel)
     '''
     The label is typically just one letter describing the type of the k-cell.
     This should be implemented in child classes. Using standard value here.
-    
-    '''  
-    
-    
-#==============================================================================
+
+    '''
+
+# =============================================================================
 #    METHODS
-#==============================================================================
-    
+# =============================================================================
+
     def updateText(self):
         '''
-        The label can only be changed in the cell, not in the reversed cell. 
-        Therefore the updateText() function is not needed to be implemented in 
+        The label can only be changed in the cell, not in the reversed cell.
+        Therefore the updateText() function is not needed to be implemented in
         the ReversedCell class, here is enough.
-        
+
         '''
         super().updateText()
         if self.myReverse:
             self.myReverse.updateText()
-            
-            
 
     def delete(self):
         '''
         To keep track of deleted cells, they are not completely removed from
         memory but marked as "deleted"
-        
+
         '''
-        
+
         self.__isDeleted = True
         self.updateText()
-    
-#==============================================================================
+
+
+# =============================================================================
 #    TEST FUNCTIONS
-#==============================================================================
+# =============================================================================
+
+
 if __name__ == "__main__":
     with MyLogging('SuperCell'):
-        
+
         cc.printBlue('Create Super Cell')
         testSUPC = SuperCell()
-        
-        cc.printBlue('Check if both, Super Cell and its reverse, have been created')
-        print(testSUPC,-testSUPC)
-    
 
+        cc.printBlue('Check if both, Super Cell and its reverse, ' +
+                     'have been created')
+        print(testSUPC, -testSUPC)
