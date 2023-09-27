9e337fa3f28aafa3a593c37d2252bbb569d4c7c9
Tobias Scheuermann <mail@tobias-scheuermann.de>
2023-09-27 11:03:32 +0200

    Formatted reversedSimpleCell file according to pep8

--- kCells/cell/reversedSimpleCell.py
+++ kCells/cell/reversedSimpleCell.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-#==============================================================================
+# =============================================================================
 # REVERSED SIMPLE CELL
-#==============================================================================
+# =============================================================================
 # Author:         Tobias Scheuermann
 # Institution:    Chair of Automatic Control
 #                 Department of Mechanical Engineering
@@ -13,90 +13,91 @@
 Parent class for all negative simple cells.
 
 '''
-#==============================================================================
+# =============================================================================
 #    IMPORTS
-#==============================================================================
-#-------------------------------------------------------------------------
+# =============================================================================
+# ------------------------------------------------------------------------
 #    Change to Main Directory
-#-------------------------------------------------------------------------
+# ------------------------------------------------------------------------
 import os
 if __name__ == '__main__':
     os.chdir('../../')
 
 
-#-------------------------------------------------------------------------
+# ------------------------------------------------------------------------
 #    Local Libraries
-#-------------------------------------------------------------------------
+# ------------------------------------------------------------------------
 
 #    kCells
-#--------------------------------------------------------------------
+# -------------------------------------------------------------------
 from kCells.cell.baseSimpleCell import BaseSimpleCell
 from kCells.cell.superReversedCell import SuperReversedCell
 
 
 #    Tools
-#--------------------------------------------------------------------
+# -------------------------------------------------------------------
 from tools import MyLogging
-import tools.colorConsole as cc  
+import tools.colorConsole as cc
 
 
-
-#==============================================================================
+# =============================================================================
 #    CLASS DEFINITION
-#==============================================================================
+# =============================================================================
 
-class ReversedSimpleCell(BaseSimpleCell,SuperReversedCell):
+
+class ReversedSimpleCell(BaseSimpleCell, SuperReversedCell):
     '''
-    This class inherits from the BaseSimpleCell and the SuperReversedCell classes.
-    
+    This class inherits from the BaseSimpleCell and the SuperReversedCell
+    classes.
+
     '''
 
-    
-#==============================================================================
+# =============================================================================
 #    INITIALIZATION
-#==============================================================================
-    def __init__(self,*args,**kwargs):
+# =============================================================================
+    def __init__(self, *args, **kwargs):
         '''
-        :param SuperBaseCell myReverse: 
-        :param Cell belongsTo: A cell that this simple cell is part of.       
-        :param str loggerName: The logger name needs to be passed from the 
+        :param SuperBaseCell myReverse:
+        :param Cell belongsTo: A cell that this simple cell is part of.
+        :param str loggerName: The logger name needs to be passed from the
             class at the lowest level by loggerName = __name__
-            
+
         '''
         super().__init__(**kwargs)
         self.logger.debug('Initialized ReversedSimpleCell')
-    
-        
-    
-#==============================================================================
+
+# =============================================================================
 #    SETTER AND GETTER
-#==============================================================================
-    def __getLabelSuffix(self): 
+# =============================================================================
+
+    def __getLabelSuffix(self):
         if self.myReverse:
             return self.myReverse.labelSuffix
         else:
             return super().labelSuffix
-    def __setLabelSuffix(self,l): 
+
+    def __setLabelSuffix(self, s):
         if self.myReverse:
-            self.myReverse.labelSuffix = l
+            self.myReverse.labelSuffix = s
         else:
             self.logger.error('Cannot set label suffix')
-    labelSuffix = property(__getLabelSuffix,__setLabelSuffix)
+
+    labelSuffix = property(__getLabelSuffix, __setLabelSuffix)
     '''
     The label is stored in the corresponding positive simple cell.
-    
-    '''
-    
-    
-#==============================================================================
-#    METHODS
-#==============================================================================
-    
 
-    
-#==============================================================================
+    '''
+
+# =============================================================================
+#    METHODS
+# =============================================================================
+
+
+# =============================================================================
 #    TEST FUNCTIONS
-#==============================================================================
+# =============================================================================
+
+
 if __name__ == '__main__':
     with MyLogging('reversedSimpleCell'):
         cc.printBlue('Create Reversed Simple Cell')
@@ -105,4 +106,3 @@ if __name__ == '__main__':
         print(rc1)
         cc.printBlue('Label suffix should not be settable')
         rc1.labelSuffix = '(t)'
-    
