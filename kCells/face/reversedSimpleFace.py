c3671ec7168b72ed580cbff6bf172dbb0c48a897
Tobias Scheuermann <mail@tobias-scheuermann.de>
2023-10-19 19:03:34 +0200

    Formatted reversedSimpleFace file according to pep8

--- kCells/face/reversedSimpleFace.py
+++ kCells/face/reversedSimpleFace.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-#==============================================================================
+# =============================================================================
 # REVERSED SIMPLE FACE
-#==============================================================================
+# =============================================================================
 # Author:         Tobias Scheuermann
 # Institution:    Chair of Automatic Control
 #                 Department of Mechanical Engineering
@@ -9,13 +9,10 @@
 # E-Mail:         tobias.scheuermann@tum.de
 # Created on:     Tue May 22 18:15:43 2018
 
-
-
-
-#==============================================================================
+# =============================================================================
 #    IMPORTS
-#==============================================================================
-if __name__== '__main__':
+# =============================================================================
+if __name__ == '__main__':
     import os
     os.chdir('../../')
 from kCells.face.baseSimpleFace import BaseSimpleFace
@@ -23,47 +20,51 @@ from kCells.cell import ReversedSimpleCell
 import numpy as np
 
 
-#==============================================================================
+# =============================================================================
 #    CLASS DEFINITION
-#============================================================================== 
-class ReversedSimpleFace(BaseSimpleFace,ReversedSimpleCell):    
-    
-#==============================================================================
+# =============================================================================
+
+
+class ReversedSimpleFace(BaseSimpleFace, ReversedSimpleCell):
+    '''
+
+    '''
+
+# =============================================================================
 #    SLOTS
-#==============================================================================
+# =============================================================================
 #    __slots__ = ('__coordinates',
 #                 '__nodes',
 #                 '__normalVec',
 #                 '__area')
 #
 
-#==============================================================================
+# =============================================================================
 #    INITIALIZATION
-#==============================================================================
-    def __init__(self,*args,**kwargs):
-        
-        super().__init__(*args,loggerName=__name__,**kwargs)
+# =============================================================================
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, loggerName=__name__, **kwargs)
         self.__coordinates = None
         self.__area = None
         self.__normalVec = None
         self.__nodes = None
         self.logger.debug('Initialized ReversedSimpleFace')
 
-    
-    
-    
-#==============================================================================
+# =============================================================================
 #    SETTER AND GETTER
-#==============================================================================   
-    def __getCoordinates(self): 
+# =============================================================================
+    def __getCoordinates(self):
         if self.__coordinates is None:
             if self.myReverse:
                 self.__coordinates = np.flipud(self.myReverse.coordinates)
             else:
-                self.logger.error('No reverse defined, cannot give coordinates')
+                self.logger.error(
+                    'No reverse defined, cannot give coordinates')
         return self.__coordinates
+
     coordinates = property(__getCoordinates)
-    
+
     def __getNodes(self):
         if self.__nodes is None:
             if self.myReverse:
@@ -71,17 +72,20 @@ class ReversedSimpleFace(BaseSimpleFace,ReversedSimpleCell):
             else:
                 self.logger.error('No reverse defined, cannot give nodes')
         return self.__nodes
+
     nodes = property(__getNodes)
-        
+
     def __getArea(self):
         if self.__area is None:
             if self.myReverse:
-                self.__area = [[a for a in reversed(self.myReverse.area[0])],self.myReverse.area[1]]
+                self.__area = [[a for a in reversed(self.myReverse.area[0])],
+                               self.myReverse.area[1]]
             else:
                 self.logger.error('No reverse defined, cannot give area')
         return self.__area
-    area = property(__getArea)  
-    
+
+    area = property(__getArea)
+
     def __getNormalVec(self):
         if self.__normalVec is None:
             if self.myReverse:
@@ -89,32 +93,36 @@ class ReversedSimpleFace(BaseSimpleFace,ReversedSimpleCell):
             else:
                 self.logger.error('No reverse defined, cannot give normalVec')
         return self.__normalVec
-    normalVec = property(__getNormalVec)  
-    
-    
-    def __getBarycenter(self): 
+
+    normalVec = property(__getNormalVec)
+
+    def __getBarycenter(self):
         if self.myReverse:
-            return self.myReverse.barycenter 
+            return self.myReverse.barycenter
         else:
-            self.logger.error('Reversed simple face does not belong to a simple face, cannot return barycenter')
-            return np.array([0,0,0])
-    barycenter = property(__getBarycenter)  
-    
-    
-    def __getSimpleEdges(self): 
+            self.logger.error('Reversed simple face does not belong to a ' +
+                              'simple face, cannot return barycenter')
+            return np.array([0, 0, 0])
+
+    barycenter = property(__getBarycenter)
+
+    def __getSimpleEdges(self):
         if self.myReverse:
-            return [se.myReverse for se in reversed(self.myReverse.simpleEdges)]
+            return [se.myReverse for se in
+                    reversed(self.myReverse.simpleEdges)]
         else:
-            self.logger.error('Reversed simple face does not belong to a simple face, cannot return simpleEdges')
+            self.logger.error(
+                'Reversed simple face does not belong to a simple face, ' +
+                'cannot return simpleEdges')
             return []
-    simpleEdges = property(__getSimpleEdges)  
-    
-#    def __getEdges(self): 
+    simpleEdges = property(__getSimpleEdges)
+
+#    def __getEdges(self):
 #        edges = []
 #        for e in self.data.edges:
 #            edges.append(-e)
 #        return list(reversed(edges))
-#    def __setEdges(self,edges):
+#    def __setEdges(self, edges):
 #        newedges = []
 #        for e in edges:
 #            newedges.append(-e)
@@ -127,21 +135,18 @@ class ReversedSimpleFace(BaseSimpleFace,ReversedSimpleCell):
 #            for e in self.__data.edges:
 #                e.addFace(self)
 #            self.createCoordinates()
-#            self.calcBarycenter()         
+#            self.calcBarycenter()
 #    edges = property(__getEdges)
 #
 #    def __getNormalVec(self): return -self.data.normalVec
 #    normalVec = property(__getNormalVec)
-    
+
+
 if __name__ == '__main__':
-    import tools.placeFigures as pf
     import tools.colorConsole as cc
     from tools import MyLogging
-    from kCells.node import Node
-    from kCells.edge import Edge
-    
-    
-    with MyLogging('Edge',debug=True) as ml:
+
+    with MyLogging('Edge', debug=True) as ml:
         rsf = ReversedSimpleFace()
         cc.printRed(rsf.coordinates)
         cc.printRed(rsf.area)
