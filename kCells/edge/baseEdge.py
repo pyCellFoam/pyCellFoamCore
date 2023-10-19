e54cc94b16c115611f9c6508d37ea020482751b1
Tobias Scheuermann <mail@tobias-scheuermann.de>
2023-10-19 18:50:22 +0200

    Formatted baseEdge file according to pep8

--- kCells/edge/baseEdge.py
+++ kCells/edge/baseEdge.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-#==============================================================================
+# =============================================================================
 # BASE EDGE
-#==============================================================================
+# =============================================================================
 # Author:         Tobias Scheuermann
 # Institution:    Chair of Automatic Control
 #                 Department of Mechanical Engineering
@@ -13,70 +13,74 @@
 
 
 '''
-#==============================================================================
+# =============================================================================
 #    IMPORTS
-#==============================================================================
-if __name__== '__main__':
+# =============================================================================
+if __name__ == '__main__':
     import os
     os.chdir('../../')
-    
+
 from kCells import BaseCell
 import tools.colorConsole as cc
 
-#==============================================================================
+
+# =============================================================================
 #    CLASS DEFINITION
-#==============================================================================
+# =============================================================================
+
 
 class BaseEdge(BaseCell):
     '''
-    
-    
+
+
     '''
-    
-#==============================================================================
+
+# =============================================================================
 #    SLOTS
-#==============================================================================
+# =============================================================================
 #    __slots__ = ()
 
-#==============================================================================
+# =============================================================================
 #    INITIALIZATION
-#==============================================================================
-    def __init__(self,*args,**kwargs):
+# =============================================================================
+    def __init__(self, *args, **kwargs):
         '''
-       
-        
-        '''
-        
-        super().__init__(*args,**kwargs)
-        self.logger.debug('Initialized BaseEdge')
-#        self.__showArrow = True
 
-        
-    
-#==============================================================================
+
+        '''
+
+        super().__init__(*args, **kwargs)
+        self.logger.debug('Initialized BaseEdge')
+
+# =============================================================================
 #    SETTER AND GETTER
-#==============================================================================
-        
-    def __getBarycenter(self): return [se.barycenter for se in self.simpleEdges]
+# =============================================================================
+
+    def __getBarycenter(self):
+        return [se.barycenter for se in self.simpleEdges]
+
     barycenter = property(__getBarycenter)
     '''
-    Returns a list of barycenters of all simple edges that the edge consists of.
-    
+    Returns a list of barycenters of all simple edges that the edge consists
+    of.
+
     '''
-    
-    def __getDirectionVec(self): return [se.directionVec for se in self.simpleEdges]
+
+    def __getDirectionVec(self):
+        return [se.directionVec for se in self.simpleEdges]
+
     directionVec = property(__getDirectionVec)
     '''
-    Returns a list of direction vectors of all simple edges that the edge 
+    Returns a list of direction vectors of all simple edges that the edge
     consists of.
-    
-    '''
 
+    '''
 
     def __getTopologicNodes(self): return [self.startNode, self.endNode]
+
     topologicNodes = property(__getTopologicNodes)
     '''
-    
+
     '''
 
     def __getLength(self):
@@ -85,98 +89,93 @@ class BaseEdge(BaseCell):
         for se in self.simpleEdges:
             lengths.append(se.length)
             totalLenth += se.length
-        return [lengths,totalLenth]
+        return [lengths, totalLenth]
     length = property(__getLength)
     '''
-    
+
     '''
-        
-    
-#==============================================================================
+
+# =============================================================================
 #    METHODS
-#==============================================================================
-    
-    
-    def intersectWithBoundingBox(self,*args,**kwargs):
+# =============================================================================
+
+    def intersectWithBoundingBox(self, *args, **kwargs):
         '''
-        
+
         '''
         nodes = []
         for se in self.simpleEdges:
-            nodes.append(se.intersectWithBoundingBox(*args,**kwargs))
+            nodes.append(se.intersectWithBoundingBox(*args, **kwargs))
         return nodes
-        
-        
-            
-#-------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------
 #    Plotting
-#-------------------------------------------------------------------------            
-    def plotEdge(self,*args,**kwargs):
+# ------------------------------------------------------------------------
+    def plotEdge(self, *args, **kwargs):
         '''
         Plot all simple edges in a given pyplot axis
-        
+
         :param ax: pyplot axis
-        
+
         '''
         if self.showInPlot:
-            
+
             if not self.simpleEdges:
-                self.logger.warning('Edge {} has no simple edges, maybe it was deleted'.format(self))
+                self.logger.warning('Edge {} '.format(self) +
+                                    'has no simple edges, ' +
+                                    'maybe it was deleted')
             for se in self.simpleEdges:
-                se.plotEdge(*args,**kwargs)  
-            
+                se.plotEdge(*args, **kwargs)
+
         else:
-            self.logger.warning('Plotting of edge {} is disabled'.format(self))  
-            
-    def plotEdgeVtk(self,*args,**kwargs):
+            self.logger.warning('Plotting of edge {} is disabled'.format(self))
+
+    def plotEdgeVtk(self, *args, **kwargs):
         '''
         Plot all simple edges in a given vtk window.
-        
+
         :param myVtk: Instance of MyVtk class
-        
+
         '''
         for se in self.simpleEdges:
-            se.plotEdgeVtk(*args,**kwargs)
-            
-            
-    def plotEdgeTikZ(self,*args,repeatLabel=True,**kwargs):
+            se.plotEdgeVtk(*args, **kwargs)
+
+    def plotEdgeTikZ(self, *args, repeatLabel=True, **kwargs):
         '''
-        
+
         '''
         if self.showInPlot:
             if repeatLabel:
                 for se in self.simpleEdges:
-                    se.plotEdgeTikZ(*args,**kwargs)
+                    se.plotEdgeTikZ(*args, **kwargs)
             else:
-                self.simpleEdges[0].plotEdgeTikZ(*args,**kwargs)
+                self.simpleEdges[0].plotEdgeTikZ(*args, **kwargs)
                 for se in self.simpleEdges[1:]:
-                    se.plotEdgeTikZ(*args,showLabel=False,**kwargs)                
-            
-            
-            
-#    def plotHeatFlow(self,ax,Q,Qmax,lmax):
-#        for se in self.simpleEdges:
-#            se.plotHeatFlow(ax,Q/len(self.simpleEdges),Qmax,lmax)
+                    se.plotEdgeTikZ(*args, showLabel=False, **kwargs)
 
-    def printEdge(self,printNodes=True):
+#    def plotHeatFlow(self, ax, Q, Qmax, lmax):
+#        for se in self.simpleEdges:
+#            se.plotHeatFlow(ax, Q/len(self.simpleEdges), Qmax, lmax)
+
+    def printEdge(self, printNodes=True):
         '''
         Prints information about the edge in the console including
-        
+
         * Number
         * Start node and end node
         * Attached faces
-        
+
         '''
         print('==========================================================')
-        cc.printBlueBackground('   ',end='')
-        print(' Edge ',self.infoText)
+        cc.printBlueBackground('   ', end='')
+        print(' Edge ', self.infoText)
         print('==========================================================')
         print()
         print('Label:'+self.labelText)
         print()
-        print('Defined by Node',self.startNode.num,'and',self.endNode.num)
+        print('Defined by Node', self.startNode.num, 'and', self.endNode.num)
         print()
-        if printNodes:            
+        if printNodes:
             print('Start Node:')
             print('-----------')
             self.startNode.printNode()
@@ -187,9 +186,9 @@ class BaseEdge(BaseCell):
             print()
         if len(self.faces) > 0:
             if len(self.faces) > 1:
-                print('this edge belongs to',len(self.faces),'Faces:')
+                print('this edge belongs to', len(self.faces), 'Faces:')
             else:
-                print('this edge belongs to',len(self.faces),'Face:')
+                print('this edge belongs to', len(self.faces), 'Face:')
             print([f.num for f in self.faces])
         else:
             print('This Edge does not belong to a Face')
@@ -199,16 +198,14 @@ class BaseEdge(BaseCell):
         print()
         print()
 
-    
 
-    
-    
-#==============================================================================
+# =============================================================================
 #    TEST FUNCTIONS
-#==============================================================================
+# =============================================================================
+
+
 if __name__ == '__main__':
     from tools import MyLogging
-    
-    with MyLogging('Cell',debug=True):
-        e1 = BaseEdge()
 
+    with MyLogging('Cell', debug=True):
+        e1 = BaseEdge()
