02f0d35561a49e188f13b98e659522944e2e9562
Tobias Scheuermann <mail@tobias-scheuermann.de>
2025-06-03 22:38:21 +0200

    Update DualEdge2D to new logger

--- pyCellFoamCore/kCells/edge/dualEdge2D.py
+++ pyCellFoamCore/kCells/edge/dualEdge2D.py
@@ -23,6 +23,12 @@ if __name__ == '__main__':
     import os
     os.chdir('../../')
 
+# ------------------------------------------------------------------------
+#    Standard Libraries
+# ------------------------------------------------------------------------
+
+import logging
+
 from kCells.cell import DualCell
 
 
@@ -31,6 +37,15 @@ from kCells.node import Node, DualNode1D, DualNode2D
 import tools.colorConsole as cc
 import tools.tumcolor as tc
 import numpy as np
+from tools.logging_formatter import set_logging_format
+
+
+# =============================================================================
+#    LOGGING
+# =============================================================================
+
+_log = logging.getLogger(__name__)
+_log.setLevel(logging.INFO)
 
 
 # =============================================================================
@@ -55,8 +70,6 @@ class DualEdge2D(Edge, DualCell):
     def __init__(self,
                  edge,
                  *args,
-                 myPrintDebug=None,
-                 myPrintError=None,
                  simplifySimpleEdges=True,
                  **kwargs):
         '''
@@ -78,34 +91,21 @@ class DualEdge2D(Edge, DualCell):
                          **kwargs)
 
         if not simplifySimpleEdges:
-            self.logger.debug('Do not simplify simple edges')
+            _log.debug('Do not simplify simple edges')
 
-        if True:
-            if myPrintDebug is None:
-                myPrintDebug = self.logger.debug
-            # myPrintInfo = self.logger.info
-            # myPrintWarning = self.logger.warning
-            if myPrintError is None:
-                myPrintError = self.logger.error
-        else:
-            self.logger.warning('Using prints instead of logger!')
-            myPrintDebug = cc.printGreen
-            # myPrintInfo = cc.printCyan
-            # myPrintWarning = cc.printYellow
-            myPrintError = cc.printRed
 
         error = True
 
         if edge.dualCell2D is not None:
-            myPrintError('Edge {} already has a dual edge!'.format(edge))
+            _log.error('Edge {} already has a dual edge!'.format(edge))
 
         if edge.dualCell1D is None:
-            myPrintDebug('{} has no dual yet --> calculating it'.format(edge))
+            _log.debug('{} has no dual yet --> calculating it'.format(edge))
             dn = DualNode1D(edge)
-            myPrintDebug('New node: {}'.format(dn))
+            _log.debug('New node: {}'.format(dn))
 
         if edge.dualCell1D is None:
-            myPrintError('Something went wrong with the calculation ' +
+            _log.error('Something went wrong with the calculation ' +
                          'of the 1D dual of {}. '.format(edge) +
                          'The duals are: 0D: {} - '.format(edge.dualCell0D) +
                          '1D: {} - '.format(edge.dualCell1D) +
@@ -113,7 +113,7 @@ class DualEdge2D(Edge, DualCell):
 
         if edge.category1 == 'border':
             if len(edge.faces) == 1:
-                myPrintDebug('Assuming 2D complex border edge')
+                _log.debug('Assuming 2D complex border edge')
 
                 self.category1 = edge.category1
                 self.category2 = edge.category2
@@ -125,9 +125,9 @@ class DualEdge2D(Edge, DualCell):
                 normv = np.linalg.norm(v)
                 if normv > self.tolerance:
                     v = v/normv
-                    myPrintDebug(v)
+                    _log.debug(v)
                     if v[2] < -0.5:
-                        myPrintDebug('Edge was in the wrong direction, ' +
+                        _log.debug('Edge was in the wrong direction, ' +
                                      'turning around')
                         self.startNode = n1
                         self.endNode = edge.dualCell1D
@@ -138,14 +138,14 @@ class DualEdge2D(Edge, DualCell):
                         v = v/np.linalg.norm(v)
 
                     if v[2] < -0.5:
-                        myPrintError('Edge {} is still in the '.format(self) +
+                        _log.error('Edge {} is still in the '.format(self) +
                                      'wrong direction')
                     else:
-                        myPrintDebug('Edge is ok')
+                        _log.debug('Edge is ok')
                         error = False
 
                 else:
-                    myPrintError('Direction vector of ' +
+                    _log.error('Direction vector of ' +
                                  'primal edge {}'.format(edge) +
                                  ': {} and '.format(edge.directionVec) +
                                  'dual edge {}:'.format(self) +
@@ -163,7 +163,7 @@ class DualEdge2D(Edge, DualCell):
                         DualNode2D(f)
 
                 if len(faces) == 2:
-                    myPrintDebug('Assuming 3D complex')
+                    _log.debug('Assuming 3D complex')
                     self.category1 = 'additionalBorder'
                     self.category2 = 'additionalBorder'
 
@@ -172,13 +172,13 @@ class DualEdge2D(Edge, DualCell):
 
                     nGeo = edge.dualCell1D
                     if nGeo is None:
-                        myPrintError('{} should have'.format(edge) +
+                        _log.error('{} should have'.format(edge) +
                                      ' a 1D dual but has not')
 
                     else:
                         self.geometricNodes = nGeo
                         self.setUp()
-                        myPrintDebug(self.directionVec)
+                        _log.debug(self.directionVec)
                         if np.linalg.norm(
                                 self.directionVec[0]-self.directionVec[1]) \
                                 < 1e-4 and simplifySimpleEdges:
@@ -186,34 +186,34 @@ class DualEdge2D(Edge, DualCell):
                             self.setUp()
                         error = False
                 else:
-                    myPrintError('Edge {} is border '.format(edge.infoText) +
+                    _log.error('Edge {} is border '.format(edge.infoText) +
                                  'and should therefor belong to ' +
                                  'two border faces, ' +
                                  'but belongs to {}: {}'.format(len(faces), faces))
 
         elif edge.category1 == 'inner':
-            myPrintDebug('Assuming 2D complex inner edge')
+            _log.debug('Assuming 2D complex inner edge')
             if len(edge.faces) == 2:
                 self.category1 = edge.category1
                 self.category2 = edge.category2
-                self.logger.debug('Creating 2D dual edge')
+                _log.debug('Creating 2D dual edge')
 
                 self.startNode = edge.faces[0].dualCell2D
                 self.endNode = edge.faces[1].dualCell2D
                 nGeo = edge.dualCell1D
                 self.geometricNodes = nGeo
                 self.setUp()
-                myPrintDebug(self.directionVec)
+                _log.debug(self.directionVec)
 
                 v = np.cross(edge.directionVec, self.directionVec)
 
                 v1 = v[0]/np.linalg.norm(v[0])
                 v2 = v[1]/np.linalg.norm(v[1])
 
-                myPrintDebug(v1, v2)
+                _log.debug(v1, v2)
 
                 if v1[2] < -0.5 or v2[2] < -0.5:
-                    myPrintDebug('Dual edge was in the wrong direction, ' +
+                    _log.debug('Dual edge was in the wrong direction, ' +
                                  'swapping start and end')
                     self.endNode = n1
                     self.startNode = edge.faces[1].dualCell2D
@@ -228,16 +228,16 @@ class DualEdge2D(Edge, DualCell):
                 error = False
 
             else:
-                self.logger.error('An inner edge in a 2D complex needs ' +
+                _log.error('An inner edge in a 2D complex needs ' +
                                   'two faces')
 
         else:
-            myPrintError('Unknown category {}'.format(edge.category) +
+            _log.error('Unknown category {}'.format(edge.category) +
                          ' of edge {}'.format(edge.infoText))
 
         if error:
             self.delete()
-            self.logger.error('An error occured during the creation process ' +
+            _log.error('An error occured during the creation process ' +
                               'of dual Edge --> deleting')
         else:
             if edge.isReverse:
@@ -255,43 +255,39 @@ class DualEdge2D(Edge, DualCell):
 #    METHODS
 # =============================================================================
 
-    def calcHeatFlow(self, myPrintDebug=None, myPrintError=None):
-        if not myPrintDebug:
-            myPrintDebug = self.logger.debug
-        if not myPrintError:
-            myPrintError = self.logger.error
+    def calcHeatFlow(self):
 
         if len(self.simpleEdges) == 1:
-            myPrintDebug('1 simple edge in dual edge {}'.format(self))
+            _log.debug('1 simple edge in dual edge {}'.format(self))
             se = self.simpleEdges[0]
             v = se.connectionVec
-            myPrintDebug('Start node dual: {} | end node dual: {}'
+            _log.debug('Start node dual: {} | end node dual: {}'
                          .format(se.startNode, se.endNode))
             if se.startNode.category == 'inner' \
                     and se.endNode.category != 'inner':
-                myPrintDebug('Using {} for interpolation'
+                _log.debug('Using {} for interpolation'
                              .format(se.startNode.dualCell2D))
                 entry = (se.startNode.num, -v[1], v[0])
             elif se.startNode.category != 'inner' \
                     and se.endNode.category == 'inner':
-                myPrintDebug('Using {} for interpolation'
+                _log.debug('Using {} for interpolation'
                              .format(se.endNode.dualCell2D))
                 entry = (se.endNode.num, -v[1], v[0])
             else:
-                myPrintError('Cannot find the correct primal face ' +
+                _log.error('Cannot find the correct primal face ' +
                              'for interpolation')
                 return None
             entries = [entry, ]
         elif len(self.simpleEdges) == 2:
-            myPrintDebug('2 simple edges in dual edge {}'.format(self))
+            _log.debug('2 simple edges in dual edge {}'.format(self))
             se1 = self.simpleEdges[0]
             se2 = self.simpleEdges[1]
             v1 = se1.connectionVec
             v2 = se2.connectionVec
 
-            myPrintDebug('Evaluating {} for simple edge {}'
+            _log.debug('Evaluating {} for simple edge {}'
                          .format(se1.startNode.dualCell2D, se1))
-            myPrintDebug('Evaluating {} for simple edge {}'
+            _log.debug('Evaluating {} for simple edge {}'
                          .format(se2.endNode.dualCell2D, se2))
             entry1 = (se1.startNode.num, -v1[1], v1[0])
             entry2 = (se2.endNode.num, -v2[1], v2[0])
@@ -299,7 +295,7 @@ class DualEdge2D(Edge, DualCell):
 
         else:
             return None
-            myPrintError('{} simple edges in dual edge {} cannot be handled!'
+            _log.error('{} simple edges in dual edge {} cannot be handled!'
                          .format(len(self.simpleEdges), self))
         return entries
 
@@ -310,140 +306,140 @@ class DualEdge2D(Edge, DualCell):
 if __name__ == "__main__":
     from kCells.node import DualNode3D
     from kCells.face import Face
-    from kCells.volume import Volume
+    from kCells.volume.volume import Volume
     import tools.placeFigures as pf
     from tools import MyLogging
-    with MyLogging('dualEdge2D'):
-        n0 = Node(0, 0, 0)
-        n1 = Node(1, 0, 0)
-        n2 = Node(0, 1, 0)
-        n3 = Node(0, 0, 1)
-        n4 = Node(1, 1, 1)
-        nodes = [n0, n1, n2, n3, n4]
+    set_logging_format(logging.DEBUG)
+    n0 = Node(0, 0, 0)
+    n1 = Node(1, 0, 0)
+    n2 = Node(0, 1, 0)
+    n3 = Node(0, 0, 1)
+    n4 = Node(1, 1, 1)
+    nodes = [n0, n1, n2, n3, n4]
 
-        e0 = Edge(n0, n1)
-        e1 = Edge(n0, n2)
-        e2 = Edge(n0, n3)
-        e3 = Edge(n1, n2)
-        e4 = Edge(n1, n3)
-        e5 = Edge(n2, n3)
-        e6 = Edge(n1, n4)
-        e7 = Edge(n2, n4)
-        e8 = Edge(n3, n4)
+    e0 = Edge(n0, n1)
+    e1 = Edge(n0, n2)
+    e2 = Edge(n0, n3)
+    e3 = Edge(n1, n2)
+    e4 = Edge(n1, n3)
+    e5 = Edge(n2, n3)
+    e6 = Edge(n1, n4)
+    e7 = Edge(n2, n4)
+    e8 = Edge(n3, n4)
 
-        edges = [e0, e1, e2, e3, e4, e5, e6, e7, e8]
+    edges = [e0, e1, e2, e3, e4, e5, e6, e7, e8]
 
-        for e in edges:
-            e.category1 = 'border'
+    for e in edges:
+        e.category1 = 'border'
 
-        f0 = Face([e0, e3, -e1])
-        f1 = Face([e1, e5, -e2])
-        f2 = Face([e0, e4, -e2])
-        f3 = Face([e3, e5, -e4])
-        f4 = Face([e4, e8, -e6])
-        f5 = Face([e5, e8, -e7])
-        f6 = Face([e3, e7, -e6])
+    f0 = Face([e0, e3, -e1])
+    f1 = Face([e1, e5, -e2])
+    f2 = Face([e0, e4, -e2])
+    f3 = Face([e3, e5, -e4])
+    f4 = Face([e4, e8, -e6])
+    f5 = Face([e5, e8, -e7])
+    f6 = Face([e3, e7, -e6])
 
-        faces = [f0, f1, f2, f3, f4, f5, f6]
-        for f in faces:
-            f.useCategory = 1
-            if f == f3:
-                f.category = 'inner'
-            else:
-                f.category = 'border'
+    faces = [f0, f1, f2, f3, f4, f5, f6]
+    for f in faces:
+        f.useCategory = 1
+        if f == f3:
+            f.category = 'inner'
+        else:
+            f.category = 'border'
 
-        v1 = Volume([-f0, -f1, f2, f3])
-        v2 = Volume([f6, f5, -f4, -f3])
-        volumes = [v1, v2]
-        for v in volumes:
-            v.useCategory = 1
-            v.category = 'inner'
+    v1 = Volume([-f0, -f1, f2, f3])
+    v2 = Volume([f6, f5, -f4, -f3])
+    volumes = [v1, v2]
+    for v in volumes:
+        v.useCategory = 1
+        v.category = 'inner'
 
-        (figs, ax) = pf.getFigures(2, 2)
-        for e in edges:
-            e.plotEdge(ax[0])
-        for f in faces:
-            f.plotFace(ax[0])
+    (figs, ax) = pf.getFigures(2, 2)
+    for e in edges:
+        e.plotEdge(ax[0])
+    for f in faces:
+        f.plotFace(ax[0])
 
-        for v in volumes:
-            v.plotVolume(ax[1])
+    for v in volumes:
+        v.plotVolume(ax[1])
 
-        dualNodes = []
-        for v in volumes:
-            dualNodes.append(DualNode3D(v))
+    dualNodes = []
+    for v in volumes:
+        dualNodes.append(DualNode3D(v))
 
-        de1 = DualEdge2D(e3)
+    de1 = DualEdge2D(e3)
 
-        for v in volumes:
-            v.showLabel = False
-            v.plotVolume(ax[2])
-        for n in dualNodes:
-            n.plotNode(ax[2])
+    for v in volumes:
+        v.showLabel = False
+        v.plotVolume(ax[2])
+    for n in dualNodes:
+        n.plotNode(ax[2])
 
-        de1.plotEdge(ax[2])
+    de1.plotEdge(ax[2])
 
-        n100 = Node(0, 0, 0, num=100)
-        n101 = Node(1, 0, 0, num=101)
-        n102 = Node(1, 1, 0, num=102)
-        n103 = Node(0, 1, 0, num=103)
-        n104 = Node(2, 0, 0, num=104)
-        n105 = Node(2, 1, 0, num=105)
-        n106 = Node(0, 2, 0, num=106)
-        n107 = Node(2, 2, 0, num=107)
-        n108 = Node(1, 1.5, 0, num=108)
-        nodes = [n100, n101, n102, n103, n104, n105, n106, n107, n108]
+    n100 = Node(0, 0, 0, num=100)
+    n101 = Node(1, 0, 0, num=101)
+    n102 = Node(1, 1, 0, num=102)
+    n103 = Node(0, 1, 0, num=103)
+    n104 = Node(2, 0, 0, num=104)
+    n105 = Node(2, 1, 0, num=105)
+    n106 = Node(0, 2, 0, num=106)
+    n107 = Node(2, 2, 0, num=107)
+    n108 = Node(1, 1.5, 0, num=108)
+    nodes = [n100, n101, n102, n103, n104, n105, n106, n107, n108]
 
-        e100 = Edge(n100, n101, num=100)
-        e101 = Edge(n101, n102, num=101)
-        e102 = Edge(n102, n103, num=102)
-        e103 = Edge(n103, n100, num=103)
-        e104 = Edge(n101, n104, num=104)
-        e105 = Edge(n104, n105, num=105)
-        e106 = Edge(n105, n102, num=106)
-        e107 = Edge(n101, n105, num=107)
-        e108 = Edge(n103, n106, num=108)
-        e109 = Edge(n102, n108, num=109)
-        e110 = Edge(n108, n106, num=110)
-        e111 = Edge(n105, n107, num=111)
-        e112 = Edge(n107, n108, num=112)
-        e113 = Edge(n107, n106, num=113)
-        edges = [e100, e101, e102, e103, e104, e105, e106, e107, e108, e109,
-                 e110, e111, e112, e113]
+    e100 = Edge(n100, n101, num=100)
+    e101 = Edge(n101, n102, num=101)
+    e102 = Edge(n102, n103, num=102)
+    e103 = Edge(n103, n100, num=103)
+    e104 = Edge(n101, n104, num=104)
+    e105 = Edge(n104, n105, num=105)
+    e106 = Edge(n105, n102, num=106)
+    e107 = Edge(n101, n105, num=107)
+    e108 = Edge(n103, n106, num=108)
+    e109 = Edge(n102, n108, num=109)
+    e110 = Edge(n108, n106, num=110)
+    e111 = Edge(n105, n107, num=111)
+    e112 = Edge(n107, n108, num=112)
+    e113 = Edge(n107, n106, num=113)
+    edges = [e100, e101, e102, e103, e104, e105, e106, e107, e108, e109,
+             e110, e111, e112, e113]
 
-        borderEdges = [e100, e104, e105, e111, e113, e108, e103]
-        innerEdges = [e for e in edges if e not in borderEdges]
+    borderEdges = [e100, e104, e105, e111, e113, e108, e103]
+    innerEdges = [e for e in edges if e not in borderEdges]
 
-        for e in borderEdges:
-            e.category1 = 'border'
-        for e in innerEdges:
-            e.category1 = 'inner'
+    for e in borderEdges:
+        e.category1 = 'border'
+    for e in innerEdges:
+        e.category1 = 'inner'
 
-        f100 = Face([e100, e101, e102, e103])
-        f101 = Face([e104, e105, -e107])
-        f102 = Face([e107, e106, -e101])
-        f103 = Face([e109, e110, -e108, -e102])
-        f104 = Face([e113, -e110, -e112])
-        f105 = Face([e111, e112, -e109, -e106])
-        faces = [f100, f101, f102, f103, f104, f105]
+    f100 = Face([e100, e101, e102, e103])
+    f101 = Face([e104, e105, -e107])
+    f102 = Face([e107, e106, -e101])
+    f103 = Face([e109, e110, -e108, -e102])
+    f104 = Face([e113, -e110, -e112])
+    f105 = Face([e111, e112, -e109, -e106])
+    faces = [f100, f101, f102, f103, f104, f105]
 
-        dualNodes = []
-        for f in faces:
-            f.category1 = 'inner'
-            dualNodes.append(DualNode2D(f))
+    dualNodes = []
+    for f in faces:
+        f.category1 = 'inner'
+        dualNodes.append(DualNode2D(f))
 
-        for e in edges:
-            e.plotEdge(ax[3])
-        for f in faces:
-            f.plotFace(ax[3])
+    for e in edges:
+        e.plotEdge(ax[3])
+    for f in faces:
+        f.plotFace(ax[3])
 
-        ax[3].view_init(90, -90)
-        ax[3].set_xlabel('x')
-        ax[3].set_ylabel('y')
+    ax[3].view_init(90, -90)
+    ax[3].set_xlabel('x')
+    ax[3].set_ylabel('y')
 
-        dualEdges = []
-        for e in edges:
-            dualEdges.append(DualEdge2D(e))
+    dualEdges = []
+    for e in edges:
+        dualEdges.append(DualEdge2D(e))
 
-        for e in dualEdges:
-            e.color = tc.TUMOrange()
-            e.plotEdge(ax[3])
+    for e in dualEdges:
+        e.color = tc.TUMOrange()
+        e.plotEdge(ax[3])
