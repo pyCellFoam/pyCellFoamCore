4c9ae6693fe6332524a4a284d43b79c1dcf29a20
Tobias Scheuermann <mail@tobias-scheuermann.de>
2025-12-27 08:57:30 +0100

    Refactor docstrings and logging format in BaseCell class for consistency

--- pyCellFoamCore/k_cells/cell/base_cell.py
+++ pyCellFoamCore/k_cells/cell/base_cell.py
@@ -9,10 +9,10 @@
 # E-Mail:         tobias.scheuermann@tum.de
 # Created on:     Fri Sep 21 15:17:19 2018
 
-'''
+"""
 Parent class for all k-Cells.
 
-'''
+"""
 # =============================================================================
 #    IMPORTS
 # =============================================================================
@@ -54,47 +54,53 @@ _log = logging.getLogger(__name__)
 _log.setLevel(logging.INFO)
 
 # =============================================================================
-#    CLASS DEFINITION
+#    CLASS BASE CELL
 # =============================================================================
 
 
 class BaseCell(SuperBaseCell):
-    '''
+    """
     This class only inherits from the SuperBaseCell class.
 
-    '''
+    """
+    # ------------------------------------------------------------------------
+    #    Slots
+    # ------------------------------------------------------------------------
+    __slots__ = (
+        "__tikz_label_position",
+    )
 
-# =============================================================================
-#    INITIALIZATION
-# =============================================================================
-    def __init__(self, *args, tikZLabelPosition='below right', **kwargs):
-        '''
+    # ------------------------------------------------------------------------
+    #    Initialization
+    # ------------------------------------------------------------------------
+    def __init__(self, *args, tikZLabelPosition="below right", **kwargs):
+        """
 
         :param SuperBaseCell my_reverse:
         :param str loggerName: The logger name needs to be passed from the
             class at the lowest level by loggerName = __name__
 
-        '''
+        """
 
         super().__init__(*args, **kwargs)
         if self.__check_tikz_label_position(tikZLabelPosition):
             self.__tikz_label_position = tikZLabelPosition
         else:
-            self.__tikz_label_position = 'below right'
+            self.__tikz_label_position = "below right"
 
-        _log.debug('Initialized BaseCell')
+        _log.debug("Initialized BaseCell")
 
 # =============================================================================
 #    SETTER AND GETTER
 # =============================================================================
 
     def __get_label_suffix(self):
-        return ''
+        return ""
     label_suffix = property(__get_label_suffix)
-    '''
+    """
     k-cells  have no suffix (only simple cells do)
 
-    '''
+    """
 
     def __get_tikz_label_position(self):
         return self.__tikz_label_position
@@ -114,12 +120,12 @@ class BaseCell(SuperBaseCell):
 
     def __check_tikz_label_position(self, label_position):
         if label_position in [
-            'left', 'right', 'above', 'below', 'below right', 'below left',
-            'above right', 'above left'
+            "left", "right", "above", "below", "below right", "below left",
+            "above right", "above left"
         ]:
             return True
 
-        _log.error('Unknown position for TikZ label %s', label_position)
+        _log.error("Unknown position for TikZ label %s", label_position)
         return False
 
 
@@ -128,10 +134,10 @@ class BaseCell(SuperBaseCell):
 # =============================================================================
 
 class BaseCellPlotly:
-    '''
+    """
     Class for fast plotting of k-Cells using Plotly.
 
-    '''
+    """
 
     def __init__(self):
         pass
@@ -149,23 +155,23 @@ class BaseCellPlotly:
         #         "yaxis_title": "Y Axis",
         #         "zaxis_title": "Z Axis",
         #     },
-        #     paper_bgcolor='white',
-        #     plot_bgcolor='white'
+        #     paper_bgcolor="white",
+        #     plot_bgcolor="white"
         # )
 
         # plotly_figure.update_layout(
         #     scene=dict(
-        #         xaxis_title='X Axis',
-        #         yaxis_title='Y Axis',
-        #         zaxis_title='Z Axis',
+        #         xaxis_title="X Axis",
+        #         yaxis_title="Y Axis",
+        #         zaxis_title="Z Axis",
         #         camera=dict(
         #             up=dict(x=0, y=0, z=1),
         #             center=dict(x=0, y=0, z=0),
         #             eye=dict(x=1.25, y=1.25, z=1.25)
         #         ),
-        #         dragmode='orbit'
+        #         dragmode="orbit"
         #     ),
-        #     scene_camera_projection=dict(type='perspective')
+        #     scene_camera_projection=dict(type="perspective")
         # )
 
         plotly_figure.update_layout(
@@ -184,8 +190,8 @@ class BaseCellPlotly:
                 # "zaxis": {"visible": show_axes},
             },
             scene_camera_projection={"type": "perspective"},
-            paper_bgcolor='white',
-            plot_bgcolor='white'
+            paper_bgcolor="white",
+            plot_bgcolor="white"
         )
 
         if show_axes is not None:
@@ -206,7 +212,7 @@ class BaseCellPlotly:
 # =============================================================================
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     set_logging_format(logging.DEBUG)
 
     testBC = BaseCell()
